5c1df9accef594a00a639a16b06ba78d85ef0cf2
Poncho <ameneses67@gmail.com>
2023-03-31 16:55:20 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import "./style.css";
+
+const menuOpen = document.querySelector(".open-menu") as HTMLImageElement;
+const menuClose = document.querySelector(".close-menu") as HTMLImageElement;
+const navBar = document.querySelector(".navbar") as HTMLElement;
+const backdrop = document.querySelector(".backdrop") as HTMLDivElement;
+
+menuOpen.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
+
+function openMenu() {
+  navBar.style.scale = "1 1";
+  // navBar.style.display = "block";
+  backdrop.style.scale = "1 1";
+}
+
+function closeMenu() {
+  navBar.style.scale = "0 1";
+  // navBar.style.display = "none";
+  backdrop.style.scale = "0 1";
+}
